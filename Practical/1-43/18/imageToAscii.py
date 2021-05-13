23cd68c33ddacdee03d18883e56406f040efe050
William Riddell <william.riddell@protonmail.com>
2021-05-13 02:31:09 +0100

    working generator
    Generation working although res is way to big, now working on chunking pixels and then allocating values

--- Practical/1-43/18/imageToAscii.py
+++ Practical/1-43/18/imageToAscii.py
@@ -2,7 +2,7 @@ import sys, random, argparse
 import numpy as np
 import math
 from PIL import Image
-gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. " #72 values
 
 # A method is to sample the image down 
 # to grayscale with less than 8-bit 
@@ -12,12 +12,24 @@ def ImageToAscii(fileName):
     print("Generating image for file: " + fileName) 
     #get file and convert to gray scale (pillow)
     im = Image.open(fileName).convert('LA')
-    return assignCharater(im)
-
-def assignCharater(im):
+    im = im.convert ('RGB')
     width, height = im.size
+    #create string to store chars
+    ascii = ""
+    #for each pixel, compute average brightness
+    for x in range(width):
+        for y in range(height):
+            r,g,b = im.getpixel((x,y))
+            brightness = (r+g+b)/3 #0 is dark (black) and 255 is bright (white)
+            #find which value will represent the pixel
+            location = int((brightness//3.541)-3)
+            pixelChar = scale[location]
+            ascii += pixelChar
+        ascii += '\n'
+    return ascii 
 
 def main():
-    print(ImageToAscii(sys.argv[1]))
+    textFile = open("output.txt", "w")
+    textFile.write(ImageToAscii(sys.argv[1]))
 
 main()
